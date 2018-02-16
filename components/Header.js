00212eabac4a0ea0fcfbcaf1b9099b2531e69d5a
Anne-Marie Esteves <amesteves@octo.com>
2018-02-16 13:41:41 +0100

    correct indentation

--- components/Header.js
+++ components/Header.js
@@ -2,20 +2,19 @@ import Link from 'next/link';
 import React from 'react';
 
 const Header = () => (
-	<header>
+  <header>
     <nav className="nav">
       <div className="nav__container">
         <a className="nav__link" href="index.html">
           <img className="nav__logo" src="static/images/logo_site.svg" alt="Accueil de particulier.api.data.gouv.fr" />
         </a>
-      <ul className="nav__links">
-        <li><Link href="/"><a>Home</a></Link></li>
-        <li><Link href="/about"><a>About</a></Link></li>
-      </ul>
+        <ul className="nav__links">
+          <li><Link href="/"><a>Home</a></Link></li>
+          <li><Link href="/about"><a>About</a></Link></li>
+        </ul>
       </div>
     </nav>
-
- </header>
+  </header>
 );
 
 export default Header;
