f3968cf716214f32fdc2f8c15ee967cdc22af91b
Anne-Marie Esteves <amesteves@octo.com>
2018-02-26 16:43:19 +0100

    add api.gouv logo and update css

--- components/footer.js
+++ components/footer.js
@@ -3,12 +3,13 @@ import React from 'react'
 const Footer = () => (
   <footer className='footer'>
     <div className='container'>
-      <div className='footer__logo'>
-        <img src='/static/images/etalab.svg' alt='Logo Etalab' />
+      <div className='footer__logo' width='200px'>
+        <a href='https://api.gouv.fr/'>
+          <img src='/static/images/logo-api.gouv.fr-white.png' alt='Logo Beta Gouv' />
+        </a>
         <ul className='footer__social'>
-          <li><a href='https://twitter.com/etalab' title='Twitter'><img src='/static/images/social/twitter.svg' alt='Twitter' className='icon icon-twitter' /></a></li>
-          <li><a href='https://github.com/etalab' title='Github'><img src='/static/images/social/github.svg' alt='Github' className='icon icon-github' /></a></li>
-          <li><a href='https://www.facebook.com/etalab' title='Facebook'><img src='/static/images/social/facebook.svg' alt='Facebook' className='icon icon-facebook' /></a></li>
+          <li><a href='https://twitter.com/BetaGouv' title='Twitter'><img src='/static/images/social/twitter.svg' alt='Twitter' className='icon icon-twitter' /></a></li>
+          <li><a href='https://github.com/betagouv' title='Github'><img src='/static/images/social/github.svg' alt='Github' className='icon icon-github' /></a></li>
           <li><a href='mailto:contact@particulier.api.gouv.fr' title='Nous écrire un mail'><img src='/static/images/social/email.svg' alt='Email' className='icon icon-mail' /></a></li>
         </ul>
       </div>
