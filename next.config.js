b0582c095595d68b666eb557a12138b56fc9d793
Anne-Marie Esteves <amesteves@octo.com>
2018-03-01 16:23:03 +0100

    correct file to be ok with xo linter rules

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  exportPathMap: function () {
+  exportPathMap: () => {
     return {
-      "/": { page: "/" },
-      "/about": { page: "/about" }
+      '/': {page: '/'},
+      '/about': {page: '/about'}
     }
   }
 }
