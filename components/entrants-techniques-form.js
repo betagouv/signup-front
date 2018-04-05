809e08dc3310c7fe2b64f114d8f4f5b16a18d404
Pascal Knoth <pknoth@octo.com>
2018-04-05 14:33:04 +0200

    certificates for technical inputs

--- components/entrants-techniques-form.js
+++ components/entrants-techniques-form.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Router from 'next/router'
 import Utils from '../lib/utils'
 import Services from '../lib/services'
+import {BACK_HOST} from '@env'
+const axios = require('axios')
 
 class EntrantsTecniquesForm extends React.Component {
   constructor(props) {
@@ -9,11 +11,39 @@ class EntrantsTecniquesForm extends React.Component {
 
     this.state = {
       enrollment: {
-        ips_de_production: ''
+        documents: [],
+        ips_de_production: '',
+        autorite_certification: ''
       }
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.upload = this.upload.bind(this)
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({enrollment: this.props.enrollment})
+    }, 3000)
+  }
+
+  upload(event) {
+    const {enrollment} = this.props
+
+    const data = new FormData()
+    data.append('enrollment[documents_attributes][][attachment]', event.target.files[0])
+    data.append('enrollment[documents_attributes][][type]', 'Document::ProductionCertificatePublicKey')
+
+
+    let token
+    if (typeof localStorage) token = localStorage.getItem('token')
+    axios.patch(BACK_HOST + '/api/enrollments/' + enrollment.id, data, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => this.setState({enrollment: response.data}))
+
   }
 
   handleChange(event) {
@@ -28,14 +58,12 @@ class EntrantsTecniquesForm extends React.Component {
   }
 
   handleSubmit(event) {
-    const token = localStorage.getItem('token')
-    const componentState = this.state
-    const {enrollment} = this.props
-    Object.assign(enrollment, componentState.enrollment)
-    console.log(enrollment)
+    let token
+    if (typeof localStorage) token = localStorage.getItem('token')
+    const {enrollment} = this.state
 
     Services.triggerUserEnrollment('send_technical_inputs', enrollment, token).then(response => {
-        Router.push('/demandes')
+        if (response.status == 200) Router.push('/demandes')
     }).catch((error) => {
       if (error.response.status === 422) {
         alert('Formulaire incomplet' + response.request.response)
@@ -49,6 +77,11 @@ class EntrantsTecniquesForm extends React.Component {
   }
 
   render() {
+    let token
+    if (typeof localStorage !== 'undefined') token = localStorage.getItem('token')
+    const {enrollment} = this.state
+
+    const productionCertificate = enrollment.documents.filter((e) => e.type == 'Document::ProductionCertificatePublicKey')[0]
     return (
       <div className='main-pane'>
         <form onSubmit={this.handleSubmit}>
@@ -57,8 +90,19 @@ class EntrantsTecniquesForm extends React.Component {
             <p>Afin de mettre en production votre application et la connexion à API Impôts Particulier veuillez remplir les entrants techniques suivants</p>
           </section>
           <div className='form__group'>
-            <label htmlFor='ips_de_production'>IPs de production <i>(séparés par un espace)</i></label>
-            <input type='text' onChange={this.handleChange} name='enrollment.ips_de_production' id='ips_de_production'/>
+            <label htmlFor='production_certificate'>Certificat de production</label>
+            {productionCertificate &&
+              <a href={BACK_HOST + productionCertificate.attachment.url + '?token=' + token}>certificat</a>
+            }
+            <input type='file' onChange={this.upload} name='enrollment.production_certificate' id='production_certificate'/>
+          </div>
+          <div className='form__group'>
+            <label htmlFor='autorite_certification'>Autorité de certification</label>
+            <input type='text' onChange={this.handleChange} name='enrollment.autorite_certification' id='autorite_certification' value={enrollment.autorite_certification} />
+          </div>
+          <div className='form__group'>
+            <label htmlFor='ips_de_production'>IPs de production</label>
+            <input type='text' onChange={this.handleChange} name='enrollment.ips_de_production' id='ips_de_production' value={enrollment.ips_de_production} />
           </div>
           <div className='button-list'>
             <button className='button' type='submit' name='subscribe' id='submit'>Envoyer les entrants techniques</button>
