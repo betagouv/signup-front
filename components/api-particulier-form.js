2e34a87bb3c7538cd7de3220a6e458ae83dc5b33
Anne-Marie Esteves <amesteves@octo.com>
2018-04-17 14:54:38 +0200

    add donnes part in the form, still missing component convention css

--- components/api-particulier-form.js
+++ components/api-particulier-form.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Router from 'next/router'
 import Utils from '../lib/utils'
 import Services from '../lib/services'
+import Convention from './convention'
 
 class ApiParticulierForm extends React.Component {
   constructor(props) {
@@ -13,6 +14,12 @@ class ApiParticulierForm extends React.Component {
         texte_fondement_juridique: '', // eslint-disable-line camelcase
         url_fondement_juridique: '', // eslint-disable-line camelcase
         validation_de_convention: false, // eslint-disable-line camelcase
+        information_DPO_confirmee: false, // eslint-disable-line camelcase
+        scope_dgfip_avis_imposition: '', // eslint-disable-line camelcase
+        scope_cnaf_attestation_droits: '', // eslint-disable-line camelcase
+        scope_cnaf_quotient_familial: '', // eslint-disable-line camelcase
+        duree_conservation_donnees: '', // eslint-disable-line camelcase
+        DPO_de_convention: false, // eslint-disable-line camelcase
         fournisseur_de_donnees: 'api-particulier' // eslint-disable-line camelcase
       }
     }
@@ -98,6 +105,41 @@ class ApiParticulierForm extends React.Component {
           <h1 id='donnees'>Données</h1>
           <section className='information-text'>texte descriptif</section>
 
+          <div className='form__group'>
+            <fieldset className='vertical'>
+              <legend>Sélectionnez vos jeux de données souhaités</legend>
+              <div>
+                <input onChange={this.handleChange} checked={enrollment.scope_dgfip_avis_imposition ? 'checked' : false} type='checkbox' name='enrollment.scope_dgfip_avis_imposition' id='checkbox-scope_dgfip_avis_imposition' value='true' />
+                <label htmlFor='checkbox-scope_dgfip_avis_imposition' className='label-inline'>DGFiP - Avis Imposition</label>
+              </div>
+              <div>
+                <input onChange={this.handleChange} checked={enrollment.scope_cnaf_attestation_droits ? 'checked' : false} type='checkbox' name='enrollment.scope_cnaf_attestation_droits' id='checkbox-scope_cnaf_attestation_droits' value='scope_cnaf_attestation_droits' />
+                <label htmlFor='checkbox-scope_cnaf_attestation_droits' className='label-inline'>CNAF - Attestation de droits</label>
+              </div>
+              <div>
+                <input onChange={this.handleChange} checked={enrollment.scope_cnaf_quotient_familial ? 'checked' : false} type='checkbox' name='enrollment.scope_cnaf_quotient_familial' id='checkbox-scope_cnaf_quotient_familial' value='true' />
+                <label htmlFor='checkbox-scope_cnaf_quotient_familial' className='label-inline'>CNAF - Quotient Familial</label>
+              </div>
+            </fieldset>
+          </div>
+          <div className='form__group'>
+            <label htmlFor='duree_conservation_donnees'>Durée de conservation des données (en mois, années ??)</label>
+            <input type='text' onChange={this.handleChange} name='enrollment.duree_conservation_donnees' id='duree_conservation_donnees' value={enrollment.url_fondement_juridique} />
+          </div>
+          <div className='form__group'>
+            <label htmlFor='texte_fondement_juridique'>Destinataires des données</label>
+            <textarea onChange={this.handleChange} name='enrollment.texte_fondement_juridique' id='texte_fondement_juridique' value={enrollment.texte_fondement_juridique} />
+          </div>
+          <section className='information-text'>Pour mémoire, seuls les agents dûment habilités pour traiter une démarches doivent accéder aux données transmises.</section>
+          <Convention />
+          <div className='form__group'>
+            <input onChange={this.handleChange} checked={enrollment.validation_de_convention} type='checkbox' name='enrollment.validation_de_convention' id='validation_de_convention' />
+            <label htmlFor='validation_de_convention' className='label-inline'>J&lsquo;ai compris et j&lsquo;accepte <a href='/static/docs/convention.pdf'>la convention d’adhésion à API Particulier</a></label>
+          </div>
+          <div className='form__group'>
+            <input onChange={this.handleChange} checked={enrollment.information_DPO_confirmee} type='checkbox' name='enrollment.information_DPO_confirmee' id='information_DPO_confirmee' />
+            <label htmlFor='information_DPO_confirmee' className='label-inline'> Je confirme que le DPO de mon organisme est informé de la présente demande</label>
+          </div>
         </form>
 
       </div>
