3e31b09ec0cae1ee879dc8b2630c3b1378e0a7b3
Pascal Knoth <pknoth@octo.com>
2018-05-02 14:23:21 +0200

    lint

--- components/redirect.js
+++ components/redirect.js
@@ -1,31 +1,44 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Router from 'next/router'
 
 class Redirect extends React.Component {
   constructor(props) {
     super(props)
 
-    if(this.props.redirect) {
+    const {redirect} = this.props
+
+    if (redirect) {
       this.state = {redirect: true}
     } else {
       this.state = {redirect: false}
     }
   }
+
   componentDidMount() {
     const storedPath = localStorage.getItem('redirect') || '/'
+    const {redirect, pathName} = this.state
 
-    if (this.state.redirect) {
+    if (redirect) {
       Router.push(storedPath)
 
       localStorage.removeItem('redirect')
     } else {
-      localStorage.setItem('redirect', this.props.pathName || '')
+      localStorage.setItem('redirect', pathName || '')
     }
   }
 
   render() {
-    return <div></div>
+    return <div className='redirect' />
   }
+}
+
+Redirect.propTypes = {
+  redirect: PropTypes.bool
+}
+
+Redirect.defaultProps = {
+  redirect: false
 }
 
 export default Redirect
