0112c24396810157c371ce92a2e71d6c6f91e6e2
Pascal Knoth <pknoth@octo.com>
2018-05-28 13:45:04 +0200

    add a "new" button on enrollment listing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Link from 'next/link'
 import Page from '../components/page'
 import EnrollmentTable from '../components/enrollment-table'
 import Redirect from '../components/redirect'
@@ -15,6 +16,13 @@ class Index extends React.Component {
             <div className='container'>
               <h1>Liste des demandes</h1>
               <EnrollmentTable />
+              <p className='text-right'>
+                <Link href={{pathname: '/api-particulier.html'}}>
+                  <a className='button large' name='nouvelle-demande'>
+                    Nouvelle Demande
+                  </a>
+                </Link>
+              </p>
             </div>
           </section>
         </Page>
