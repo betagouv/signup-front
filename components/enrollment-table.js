9f4c6ef0fea5674a2e64e9a33ac17832a2b513d8
Raphaël Dubigny <rdubigny@gmail.com>
2018-06-25 16:12:47 +0200

    refactor enrollment components

--- components/enrollment-table.js
+++ components/enrollment-table.js
@@ -5,25 +5,24 @@ import withUser from '../components/hoc/with-user'
 import Services from '../lib/services'
 import Enrollment from '../components/enrollment'
 
-export const STATE_HUMAN_NAMES = {
-  pending: 'En attente de soumission',
-  sent: 'En attente de validation',
-  validated: 'Demande validée',
-  refused: 'Demande refusée',
-  technical_inputs: 'En attente de déploiement', // eslint-disable-line camelcase
-  deployed: 'Déployé'
-}
-
-const FOURNISSEUR_DE_DONNEES_HUMAN_NAMES = {
-  'api-particulier': 'API Particulier',
-  'api-entreprise': 'API Entreprise',
-  dgfip: 'API Impôts particulier'
-}
+const TABLE_HEADERS = [
+  {
+    Header: 'Fournisseur de service',
+    accessor: 'demarche.intitule'
+  }, {
+    Header: 'Type de demande',
+    accessor: 'human_fournisseur_de_donnees'
+  }, {
+    Header: 'Statut',
+    accessor: 'human_state'
+  }
+]
 
 class EnrollmentTable extends React.Component {
   constructor(props) {
     super(props)
 
+    this.triggerAction = this.triggerAction.bind(this)
     this.state = {
       enrollments: []
     }
@@ -32,44 +31,26 @@ class EnrollmentTable extends React.Component {
   componentDidMount() {
     Services.getUserEnrollments().then(enrollments => {
       this.setState({enrollments: enrollments.map(enrollment => {
-        enrollment.human_state = STATE_HUMAN_NAMES[enrollment.state] // eslint-disable-line camelcase
-        enrollment.human_fournisseur_de_donnees = FOURNISSEUR_DE_DONNEES_HUMAN_NAMES[enrollment.fournisseur_de_donnees] // eslint-disable-line camelcase
         return enrollment
       })})
     })
   }
 
-  trigger(action, enrollment) {
-    const currentEnrollments = this.state.enrollments // eslint-disable-line react/destructuring-assignment
-    return () => Services.triggerUserEnrollment(action, enrollment).then(({data: updatedEnrollment}) => {
-      if (updatedEnrollment) {
-        updatedEnrollment.human_state = STATE_HUMAN_NAMES[updatedEnrollment.state] // eslint-disable-line camelcase
-
-        const updatedEnrollments = currentEnrollments.map(currentEnrollment => {
-          if (updatedEnrollment.id === currentEnrollment.id) {
-            return updatedEnrollment
-          }
-          return currentEnrollment
-        })
-        this.setState({enrollments: updatedEnrollments})
-      }
+  triggerAction = (enrollment, action) => {
+    return Services.triggerUserEnrollment(action, enrollment).then(({data: updatedEnrollment}) => {
+      const currentEnrollments = this.state.enrollments
+      const updatedEnrollments = currentEnrollments.map(currentEnrollment => {
+        if (updatedEnrollment.id === currentEnrollment.id) {
+          return updatedEnrollment
+        }
+        return currentEnrollment
+      })
+      this.setState({enrollments: updatedEnrollments})
     })
   }
 
   render() {
     const {enrollments} = this.state
-    const columns = [
-      {
-        Header: 'Fournisseur de service',
-        accessor: 'demarche.intitule'
-      }, {
-        Header: 'Type de demande',
-        accessor: 'human_fournisseur_de_donnees'
-      }, {
-        Header: 'Statut',
-        accessor: 'human_state'
-      }
-    ]
 
     const pageSize = 10
 
@@ -80,8 +61,8 @@ class EnrollmentTable extends React.Component {
         </Head>
         <ReactTable
           data={enrollments}
-          columns={columns}
-          expanded={Array(pageSize).fill(true)} // expand all
+          columns={TABLE_HEADERS}
+          expanded={new Array(pageSize).fill(true)} // Expand all
           showPageSizeOptions={false}
           pageSize={pageSize}
           previousText='Précédent'
@@ -91,7 +72,7 @@ class EnrollmentTable extends React.Component {
           pageText='Page'
           ofText='sur'
           rowsText='lignes'
-          SubComponent={row => (<Enrollment title={row.original.demarche.intitule} enrollment={row.original} />)} // eslint-disable-line react/jsx-no-bind
+          SubComponent={row => (<Enrollment enrollment={row.original} triggerAction={this.triggerAction} />)}
         />
       </div>
     )
