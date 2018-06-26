2c36947dbe87bf52268188c9debf3ab2d28ddcd4
Pascal Knoth <pknoth@octo.com>
2018-06-26 14:22:28 +0200

    enrollment destructuration in render

--- components/form.js
+++ components/form.js
@@ -145,13 +145,30 @@ class Form extends React.Component {
 
   handleSirenChange(e) {
     this.handleChange(e)
-    this.getSiren()
+    // In order to call getSiren after state update from handleChange
+    setTimeout(() => this.getSiren(), 0)
   }
 
   render() {
-    const {enrollment, sirenNotFound, errors} = this.state
+    const {
+      enrollment: {
+        acl,
+        siren,
+        nom_raison_sociale,
+        adresse,
+        activite_principale,
+        contacts,
+        demarche,
+        scopes,
+        donnees,
+        validation_de_convention,
+        id
+      },
+      sirenNotFound,
+      errors
+    } = this.state
     const {form} = this.props
-    const readOnly = enrollment.acl.send_application ? false : 'disabled'
+    const readOnly = acl.send_application ? false : 'disabled'
 
     let personId = 0
     const personForm = person => {
@@ -187,7 +204,7 @@ class Form extends React.Component {
         <div className='form__group'>
           <label htmlFor='search-siren'>Rechercher votre organisme avec son SIREN</label>
           <div className='search__group'>
-            <input type='text' value={enrollment.siren} name='enrollment.siren' id='search-siren' onChange={this.handleSirenChange} />
+            <input type='text' value={siren} name='siren' id='search-siren' onChange={this.handleSirenChange} />
             <button className='overlay-button' type='button' aria-label='Recherche' onClick={this.getSiren}>
               <svg className='icon icon-search' id='icon-search' width='100%' height='100%'>
                 <title>Rechercher</title>
@@ -205,37 +222,37 @@ class Form extends React.Component {
 
         <div className='form__group'>
           <label htmlFor='nom_raison_sociale'>Raison sociale</label>
-          <input type='text' onChange={this.handleChange} name='enrollment.nom_raison_sociale' id='nom_raison_sociale' disabled value={enrollment.nom_raison_sociale} />
+          <input type='text' onChange={this.handleChange} name='nom_raison_sociale' id='nom_raison_sociale' disabled value={nom_raison_sociale} />
         </div>
         <div className='form__group'>
           <label htmlFor='adresse'>Adresse</label>
-          <input type='text' onChange={this.handleChange} name='enrollment.adresse' id='adresse' disabled value={enrollment.adresse} />
+          <input type='text' onChange={this.handleChange} name='adresse' id='adresse' disabled value={adresse} />
         </div>
         <div className='form__group'>
           <label htmlFor='activite_principale'>Code NAF</label>
-          <input type='text' onChange={this.handleChange} name='enrollment.activite_principale' id='activite_principale' disabled value={enrollment.activite_principale} />
+          <input type='text' onChange={this.handleChange} name='activite_principale' id='activite_principale' disabled value={activite_principale} />
         </div>
 
         <h3>Contacts</h3>
         <div className='row card-row'>
-          {enrollment.contacts.map(person => personForm(person))}
+          {contacts.map(person => personForm(person))}
         </div>
 
         <h2 id='demarche'>Démarche</h2>
         <section dangerouslySetInnerHTML={{__html: form.description.demarche}} className='information-text' />
         <div className='form__group'>
           <label htmlFor='intitule_demarche'>Intitulé</label>
-          <input type='text' onChange={this.handleChange} name='enrollment.demarche.intitule' id='intitule_demarche' disabled={readOnly} value={enrollment.demarche.intitule} />
+          <input type='text' onChange={this.handleChange} name='demarche.intitule' id='intitule_demarche' disabled={readOnly} value={demarche.intitule} />
         </div>
         <div className='form__group'>
           <label htmlFor='description_service'>Décrivez brièvement votre service ainsi que l&lsquo;utilisation prévue des données transmises</label>
-          <textarea rows='10' onChange={this.handleChange} name='enrollment.demarche.description' id='description_service' disabled={readOnly} value={enrollment.demarche.description} />
+          <textarea rows='10' onChange={this.handleChange} name='demarche.description' id='description_service' disabled={readOnly} value={demarche.description} />
         </div>
 
         <div className='form__group'>
           <label htmlFor='fondement_juridique'>Cadre juridique <i>(indiquez la référence ou l&apos;URL du texte vous autorisant à récolter ces données)</i></label>
 
-          <input type='text' onChange={this.handleChange} name='enrollment.demarche.fondement_juridique' id='fondement_juridique_demarche' disabled={readOnly} value={enrollment.demarche.fondement_juridique} />
+          <input type='text' onChange={this.handleChange} name='demarche.fondement_juridique' id='fondement_juridique_demarche' disabled={readOnly} value={demarche.fondement_juridique} />
         </div>
 
         <h2 id='donnees'>Données</h2>
@@ -248,12 +265,12 @@ class Form extends React.Component {
                   form.scopes.map(scope => {
                     return (
                       <div key={scope.id}>
-                        <input className='scope__checkbox' onChange={this.handleChange} type='checkbox' name={`enrollment.scopes.${scope.name}`} id={`checkbox-scope_api_entreprise${scope.name}`} disabled={readOnly} checked={enrollment.scopes[scope.name] ? 'checked' : false} />
+                        <input className='scope__checkbox' onChange={this.handleChange} type='checkbox' name={`scopes.${scope.name}`} id={`checkbox-scope_api_entreprise${scope.name}`} disabled={readOnly} checked={scopes[scope.name] ? 'checked' : false} />
                         <label htmlFor={`checkbox-scope_api_entreprise${scope.name}`} className='label-inline'>{scope.humanName}</label>
                         <div className='scope__destinataire'>
                           <div className='form__group'>
                             <label htmlFor={`destinataire_${scope.name}`}>Destinataires <a href='https://www.cnil.fr/fr/definition/destinataire' target='_blank' rel='noopener noreferrer'>(plus d&acute;infos)</a></label>
-                            <input type='text' onChange={this.handleChange} name={`enrollment.donnees.destinataires.${scope.name}`} id={`desinataire_${scope.name}`} disabled={readOnly} value={enrollment.donnees.destinataires[scope.name]} />
+                            <input type='text' onChange={this.handleChange} name={`donnees.destinataires.${scope.name}`} id={`desinataire_${scope.name}`} disabled={readOnly} value={donnees.destinataires[scope.name]} />
                           </div>
                         </div>
                       </div>
@@ -267,7 +284,7 @@ class Form extends React.Component {
 
         <div className='form__group'>
           <label htmlFor='donnees_conservation'>Conservation des données <i>(en mois)</i></label>
-          <input type='number' onChange={this.handleChange} name='enrollment.donnees.conservation' id='donnees_conservation' disabled={readOnly} value={enrollment.donnees.conservation} />
+          <input type='number' onChange={this.handleChange} name='donnees.conservation' id='donnees_conservation' disabled={readOnly} value={donnees.conservation} />
         </div>
 
         <h1 id='cgu'>Conditions d&acute;utilisation</h1>
@@ -278,16 +295,16 @@ class Form extends React.Component {
         <iframe src={form.cguLink} width='100%' height='800px' />
 
         <div className='form__group'>
-          <input onChange={this.handleChange} disabled={readOnly} checked={enrollment.validation_de_convention} type='checkbox' name='enrollment.validation_de_convention' id='validation_de_convention' />
+          <input onChange={this.handleChange} disabled={readOnly} checked={validation_de_convention} type='checkbox' name='validation_de_convention' id='validation_de_convention' />
           <label htmlFor='validation_de_convention' className='label-inline'>Je valide les présentes conditions d&apos;utilisation et confirme que le DPO de mon organisme est informé de ma demande</label>
         </div>
 
         {!readOnly &&
           <div className='button-list'>
-            {enrollment.id &&
+            {id &&
               <button className='button' type='submit' name='subscribe' id='submit'>Modifier la demande</button>
             }
-            {!enrollment.id &&
+            {!id &&
               <button className='button' type='submit' name='subscribe' id='submit'>Soumettre la demande</button>
             }
           </div>
