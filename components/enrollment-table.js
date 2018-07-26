a8c5f13d31a8a013f66fb006e648dd2ababf42d9
Raphaël Dubigny <rdubigny@gmail.com>
2018-07-26 12:09:18 +0200

    show date_fin_homologation in enrollment list for dgfip admin

--- components/enrollment-table.js
+++ components/enrollment-table.js
@@ -81,78 +81,91 @@ class EnrollmentTable extends React.Component {
     !_.isEmpty(_.pickBy(acl, (value, key) => value && this.availableAction.has(key)))
   )
 
-  columnConfiguration = [
-    {
-      // Remove arrow in sub components (see https://github.com/react-tools/react-table/issues/62#issuecomment-306061293)
-      expander: true,
-      show: false
-    },
-    {
-      id: 'updated_at',
-      accessor: ({updated_at, acl}) => ({updated_at, acl}),
-      Header: () => <ScheduleIcons />,
-      headerStyle: {...this.style.header, ...this.style.updateAtHeader},
-      style: this.style.cell,
-      width: 30,
-      Cell: ({value: {updated_at: updatedAt, acl}}) => {
-        if (!this.hasTriggerableActions({acl})) {
-          return null
-        }
-
-        const daysFromToday = moment().diff(updatedAt, 'days')
-        const color = daysFromToday > 5 ? 'red' : daysFromToday > 4 ? 'orange' : 'green'
-        return <span style={{color}}>{daysFromToday}j</span>
+  getColumnConfiguration = () => {
+    const configuration = [
+      {
+        // Remove arrow in sub components (see https://github.com/react-tools/react-table/issues/62#issuecomment-306061293)
+        expander: true,
+        show: false
       },
-      sortMethod: (firstMember, secondMember) => {
-        // Enrollment that have action triggerable by the user are shown first
-        // Then we order by last update date in order to have:
-        // 1. the oldest enrollment with triggerable action appears first
-        // 2. the oldest enrollment without triggerable action appears last
-        const firstMemberCanTriggerAction = this.hasTriggerableActions({acl: firstMember.acl})
-        const secondMemberCanTriggerAction = this.hasTriggerableActions({acl: secondMember.acl})
+      {
+        id: 'updated_at',
+        accessor: ({updated_at, acl}) => ({updated_at, acl}),
+        Header: () => <ScheduleIcons />,
+        headerStyle: {...this.style.header, ...this.style.updateAtHeader},
+        style: this.style.cell,
+        width: 30,
+        Cell: ({value: {updated_at: updatedAt, acl}}) => {
+          if (!this.hasTriggerableActions({acl})) {
+            return null
+          }
 
-        if (firstMemberCanTriggerAction && !secondMemberCanTriggerAction) {
-          return -1
+          const daysFromToday = moment().diff(updatedAt, 'days')
+          const color = daysFromToday > 5 ? 'red' : daysFromToday > 4 ? 'orange' : 'green'
+          return <span style={{color}}>{daysFromToday}j</span>
+        },
+        sortMethod: (firstMember, secondMember) => {
+          // Enrollment that have action triggerable by the user are shown first
+          // Then we order by last update date in order to have:
+          // 1. the oldest enrollment with triggerable action appears first
+          // 2. the oldest enrollment without triggerable action appears last
+          const firstMemberCanTriggerAction = this.hasTriggerableActions({acl: firstMember.acl})
+          const secondMemberCanTriggerAction = this.hasTriggerableActions({acl: secondMember.acl})
+
+          if (firstMemberCanTriggerAction && !secondMemberCanTriggerAction) {
+            return -1
+          }
+          if (!firstMemberCanTriggerAction && secondMemberCanTriggerAction) {
+            return 1
+          }
+          if (firstMember.updated_at > secondMember.updated_at) {
+            return firstMemberCanTriggerAction && secondMemberCanTriggerAction ? 1 : -1
+          }
+          if (firstMember.updated_at < secondMember.updated_at) {
+            return firstMemberCanTriggerAction && secondMemberCanTriggerAction ? -1 : 1
+          }
+          // Returning 0 or undefined will use any subsequent column sorting methods or the row index as a tiebreaker
+          return 0
         }
-        if (!firstMemberCanTriggerAction && secondMemberCanTriggerAction) {
-          return 1
-        }
-        if (firstMember.updated_at > secondMember.updated_at) {
-          return firstMemberCanTriggerAction && secondMemberCanTriggerAction ? 1 : -1
-        }
-        if (firstMember.updated_at < secondMember.updated_at) {
-          return firstMemberCanTriggerAction && secondMemberCanTriggerAction ? -1 : 1
-        }
-        // Returning 0 or undefined will use any subsequent column sorting methods or the row index as a tiebreaker
-        return 0
+      },
+      {
+        Header: 'Intitulé',
+        accessor: 'demarche.intitule',
+        headerStyle: this.style.header,
+        style: this.style.cell
+      }, {
+        Header: 'Demandeur',
+        accessor: 'applicant.email',
+        headerStyle: this.style.header,
+        style: this.style.cell
+      }, {
+        Header: 'Fournisseur',
+        id: 'fournisseur_de_donnees',
+        headerStyle: this.style.header,
+        style: {...this.style.cell, ...this.style.centeredCell},
+        width: 130,
+        accessor: ({fournisseur_de_donnees}) => (FOURNISSEUR_DE_DONNEES_LABELS[fournisseur_de_donnees])
+      }, {
+        Header: 'Statut',
+        id: 'status',
+        headerStyle: this.style.header,
+        style: {...this.style.cell, ...this.style.centeredCell},
+        width: 100,
+        accessor: ({state}) => (STATE_LABELS[state])
       }
-    },
-    {
-      Header: 'Intitulé',
-      accessor: 'demarche.intitule',
-      headerStyle: this.style.header,
-      style: this.style.cell
-    }, {
-      Header: 'Demandeur',
-      accessor: 'applicant.email',
-      headerStyle: this.style.header,
-      style: this.style.cell
-    }, {
-      Header: 'Fournisseur',
-      id: 'fournisseur_de_donnees',
-      headerStyle: this.style.header,
-      style: {...this.style.cell, ...this.style.centeredCell},
-      width: 130,
-      accessor: ({fournisseur_de_donnees}) => (FOURNISSEUR_DE_DONNEES_LABELS[fournisseur_de_donnees])
-    }, {
-      Header: 'Statut',
-      id: 'status',
-      headerStyle: this.style.header,
-      style: {...this.style.cell, ...this.style.centeredCell},
-      width: 100,
-      accessor: ({state}) => (STATE_LABELS[state])
+    ]
+
+    if (this.props.user.account_type === 'dgfip') {
+      configuration.push({
+        Header: 'Fin homologation',
+        accessor: 'date_fin_homologation',
+        headerStyle: this.style.header,
+        style: this.style.cell,
+      })
     }
-  ]
+
+    return configuration
+  }
 
   getTitle = ({column, rowInfo}) => {
     if (!rowInfo) {
@@ -179,7 +192,7 @@ class EnrollmentTable extends React.Component {
         </Head>
         <ReactTable
           data={enrollments}
-          columns={this.columnConfiguration}
+          columns={this.getColumnConfiguration()}
           defaultSorted={[
             {
               id: 'updated_at'
