630a54048590902d9c226b624c9bb4b843b39870
Raphaël Dubigny <rdubigny@gmail.com>
2018-08-07 16:53:51 +0200

    remove useless style in innerHTML

--- src/components/icons/spinner.js
+++ src/components/icons/spinner.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import styles from './spinner.css';
+import './spinner.css';
 
 export default () => (
   <React.Fragment>
     <div className="spinner" />
-    <style
-      dangerouslySetInnerHTML={{ __html: styles }} // eslint-disable-line react/no-danger
-    />
   </React.Fragment>
 );
