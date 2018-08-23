2094f4dc85330e85bc7f3a627e2e6a891f2907dd
Raphaël Dubigny <rdubigny@gmail.com>
2018-08-23 12:06:33 +0200

    set moment locale to french

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'template.data.gouv.fr/dist/style/main.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'moment/locale/fr'; // set moment locale to french globally
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
