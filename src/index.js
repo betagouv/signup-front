3d781141434a2db74b7caafde44dd07c4372761b
Raphaël Dubigny <rdubigny@gmail.com>
2018-08-24 14:22:48 +0200

    hotfix: service worker caching issue
    see https://github.com/facebook/create-react-app/issues/3574

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'template.data.gouv.fr/dist/style/main.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 import 'moment/locale/fr'; // set moment locale to french globally
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregisterServiceWorker();
