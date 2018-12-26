49e6e1144d2b3e1e04f550c494413190cea96ac6
Anna-Livia <annalivia.gomart@gmail.com>
2018-12-26 17:10:07 +0100

    Reorganize FranceConnect API Page

--- src/pages/FranceConnect.js
+++ src/pages/FranceConnect.js
@@ -3,10 +3,81 @@ import PropTypes from 'prop-types';
 import Form from '../components/Form';
 import Nav from '../components/Nav';
 
-const FranceConnectFormConfiguration = {
-  provider: 'franceconnect',
-  franceConnected: false,
-  scopes: [
+// Description du contexte
+const provider = 'franceconnect';
+const title = "Demande d'accès à FranceConnect";
+const IntroDescription = () => (
+  <div className="intro">
+    <p>
+      Pour avoir accès à FranceConnect, vous devez obtenir un agrément.
+      L&apos;accès à ce service n&apos;est pour l&apos;instant disponible que si
+      vous êtes:
+    </p>
+    <ul>
+      <li>une administration</li>
+      <li>
+        une entreprise prestataire d&apos;une administration ou ayant une
+        délégation de service public
+      </li>
+      <li>
+        un partenaire privé du service public de changement d'adresse en ligne :
+        fournisseurs d'énergie, d'eau, opérateurs de communications
+        électroniques et services postaux
+      </li>
+      <li>
+        un organisme délivrant des prestations répondant à des obligations
+        légales (assurances, banques)
+      </li>
+      <li>
+        une entreprise proposant des services dont l'usage impose la
+        vérification de l'identité ou d'un de ses attributs (majorité légale par
+        exemple)
+      </li>
+    </ul>
+    <p>
+      À la clé ? La garantie de transactions sécurisées grâce au dispositif
+      FranceConnect. Les utilisateurs quant à eux, gagneront en facilité d'accès
+      à ces nouveaux services auxquels ils pourront se connecter via
+      FranceConnect, sans avoir à créer un compte et un mot de passe.
+    </p>
+  </div>
+);
+
+// Le demandeur doit décrire le contexte d'usage de l'API
+const DemarcheDescription = () => (
+  <div className="information-text">
+    <p>
+      C&apos;est la raison pour laquelle vous collectez des données à caractère
+      personnel relatives à la gestion de l'identification, l&apos;objectif qui
+      est poursuivi par le traitement que vous mettez en place. Par exemple, «
+      se connecter au portail famille de ma ville » ou « accèder à son compte
+      personnel de mutuelle ».
+    </p>
+  </div>
+);
+const isFranceConnected = false;
+
+// Le demandeur doit donner le SIRET de son organisme
+// Le demandeur doit indiquer ses contacts
+// Le demandeur doit donner le cadre juridique qui lui donne le droit d'accès à l'API
+const CadreJuridiqueDescription = () => (
+  <div className="information-text">
+    <p>
+      Pour pouvoir bénéficier du raccordement à FranceConnect, le cadre légal et
+      réglementaire des fournisseurs de service doit permettre à la DINSIC de
+      transmettre des données fiscales à votre entité administrative.
+    </p>
+    <p>
+      Il vous est donc demandé de préciser les références du fondement légal de
+      votre droit à demander ces informations (délibération du conseil
+      municipal, décret …).
+    </p>
+  </div>
+);
+
+// Le demandeur doit séléctionner les données auxquelles il demande l'accès
+const DonneesDescription = () => <React.Fragment />;
+const donneesDisponibles = [
     {
       name: 'openid',
       humanName:
@@ -50,80 +121,12 @@ const FranceConnectFormConfiguration = {
       name: 'phone',
       humanName: 'Numéro de téléphone (information renvoyée si disponible)',
     },
-  ],
-  cguLink: '/docs/Franceconnect_CGU_FS_v3.pdf',
-  text: {
-    title: "Demande d'accès à FranceConnect",
-  },
-};
+  ];
 
-const IntroDescription = () => (
-  <div className="intro">
-    <p>
-      Pour avoir accès à FranceConnect, vous devez obtenir un agrément.
-      L&apos;accès à ce service n&apos;est pour l&apos;instant disponible que si
-      vous êtes:
-    </p>
-    <ul>
-      <li>une administration</li>
-      <li>
-        une entreprise prestataire d&apos;une administration ou ayant une
-        délégation de service public
-      </li>
-      <li>
-        un partenaire privé du service public de changement d'adresse en ligne :
-        fournisseurs d'énergie, d'eau, opérateurs de communications
-        électroniques et services postaux
-      </li>
-      <li>
-        un organisme délivrant des prestations répondant à des obligations
-        légales (assurances, banques)
-      </li>
-      <li>
-        une entreprise proposant des services dont l'usage impose la
-        vérification de l'identité ou d'un de ses attributs (majorité légale par
-        exemple)
-      </li>
-    </ul>
-    <p>
-      À la clé ? La garantie de transactions sécurisées grâce au dispositif
-      FranceConnect. Les utilisateurs quant à eux, gagneront en facilité d'accès
-      à ces nouveaux services auxquels ils pourront se connecter via
-      FranceConnect, sans avoir à créer un compte et un mot de passe.
-    </p>
-  </div>
-);
-
-const DemarcheDescription = () => (
-  <div className="information-text">
-    <p>
-      C&apos;est la raison pour laquelle vous collectez des données à caractère
-      personnel relatives à la gestion de l'identification, l&apos;objectif qui
-      est poursuivi par le traitement que vous mettez en place. Par exemple, «
-      se connecter au portail famille de ma ville » ou « accèder à son compte
-      personnel de mutuelle ».
-    </p>
-  </div>
-);
-
-const CadreJuridiqueDescription = () => (
-  <div className="information-text">
-    <p>
-      Pour pouvoir bénéficier du raccordement à FranceConnect, le cadre légal et
-      réglementaire des fournisseurs de service doit permettre à la DINSIC de
-      transmettre des données fiscales à votre entité administrative.
-    </p>
-    <p>
-      Il vous est donc demandé de préciser les références du fondement légal de
-      votre droit à demander ces informations (délibération du conseil
-      municipal, décret …).
-    </p>
-  </div>
-);
-
-const DonneesDescription = () => <React.Fragment />;
-
+// Le demandeur valide les modalités d'utilisation
 const CguDescription = () => <React.Fragment />;
+const cguLink = "/docs/Franceconnect_CGU_FS_v3.pdf"
+
 
 const FranceConnect = ({
   match: {
@@ -135,12 +138,16 @@ const FranceConnect = ({
     <div className="main-pane">
       <Form
         enrollmentId={enrollmentId}
-        form={FranceConnectFormConfiguration}
+        provider={provider}
+        title={title}
         IntroDescription={IntroDescription}
         DemarcheDescription={DemarcheDescription}
-        CguDescription={CguDescription}
+        isFranceConnected={isFranceConnected}
         CadreJuridiqueDescription={CadreJuridiqueDescription}
         DonneesDescription={DonneesDescription}
+        donneesDisponibles={donneesDisponibles}
+        CguDescription={CguDescription}
+        cguLink={cguLink}
       />
     </div>
   </div>
