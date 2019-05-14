3d2927ec02ee29c1182d0b3342523e60f0707769
Raphaël Dubigny <rdubigny@gmail.com>
2019-05-14 16:47:49 +0200

    run tests in CI

--- src/lib/tests/utils.test.js
+++ src/lib/tests/utils.test.js
@@ -1,30 +1,6 @@
-import { extractTokenFromUrl, isValidNAFCode } from '../utils';
+import { isValidNAFCode } from '../utils';
 
 describe('utils', () => {
-  describe('extractTokenFromUrl', () => {
-    describe('I have an url without hash', () => {
-      const url = 'http://localhost:3000';
-
-      it('should return nothing', () => {
-        const token = extractTokenFromUrl(url);
-        expect(token).toBe('');
-      });
-    });
-
-    describe('I have an oauth url', () => {
-      const url =
-        'http://localhost:3000/#access_token=e5498b836ef3abfb3e33c20116c06e293fca47c2970df5344cfb49353273586c&token_type=bearer&expires_in=7200';
-
-      it('should return token', () => {
-        const token = extractTokenFromUrl(url);
-
-        expect(token).toBe(
-          'e5498b836ef3abfb3e33c20116c06e293fca47c2970df5344cfb49353273586c'
-        );
-      });
-    });
-  });
-
   describe('isValidNAFCode', () => {
     it('should return true for COMMUNE D HEM', () => {
       expect(isValidNAFCode('api_particulier', '8411Z')).toBe(true);
