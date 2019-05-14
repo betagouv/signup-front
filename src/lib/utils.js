98cf9fe10a2602de866426ad13fcf240051c3967
Raphaël Dubigny <rdubigny@gmail.com>
2019-05-14 16:49:03 +0200

    display changelog with update events

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,13 @@
-import _, { isEmpty, isObject, isString } from 'lodash';
+import _, {
+  isBoolean,
+  isEmpty,
+  isObject,
+  isString,
+  mapKeys,
+  mergeWith,
+  omitBy,
+} from 'lodash';
+import flatten from 'flat';
 
 export function getErrorMessages(error) {
   if (
@@ -57,4 +66,93 @@ export function isValidNAFCode(provider, NAFcode) {
   }
 
   return true;
+}
+
+const diffFieldLabels = {
+  cgu_approved: "de l'approbation des CGU",
+  data_recipients: 'des destinataires des données',
+  data_retention_period: 'de la durée de conservation des données',
+  data_retention_comment:
+    'de la justification de la durée de conservation des données',
+  description: 'de la description',
+  fondement_juridique_title: 'de la référence du cadre juridique',
+  fondement_juridique_url: "de l'url du cadre juridique",
+  intitule: "de l'intitulé",
+  'contacts.0.nom': 'du nom du DPD',
+  'contacts.0.email': "de l'email du DPD",
+  'contacts.0.phone_number': 'du numéro de téléphone du DPD',
+  'contacts.1.nom': 'du nom du responsable de traitement',
+  'contacts.1.email': "de l'email du responsable de traitement",
+  'contacts.1.phone_number':
+    'du numéro de téléphone du responsable de traitement',
+  'contacts.2.nom': 'du nom du responsable technique',
+  'contacts.2.email': "de l'email du responsable technique",
+  'contacts.2.phone_number': 'du numéro de téléphone du responsable technique',
+};
+
+function flattenDiffTransformer(accumulatorObject, fullObjectDiff, objectKey) {
+  if (typeof fullObjectDiff[0] !== 'object') {
+    accumulatorObject[objectKey] = fullObjectDiff;
+
+    return accumulatorObject;
+  }
+  // {contacts: [[{'name': 'c', email: 'd'}], [{'name': 'e', email: 'd'}]]}
+  const objectBefore = flatten(fullObjectDiff[0], objectKey);
+  const objectAfter = flatten(fullObjectDiff[1], objectKey);
+  const objectDiff = mergeWith(
+    objectBefore,
+    objectAfter,
+    (valueBefore, valueAfter) => [valueBefore, valueAfter]
+  );
+  // {0.name: ['c', 'e'], 0.email: ['d', 'd']}
+  const objectDiffNoUnchanged = omitBy(
+    objectDiff,
+    value => value[0] === value[1]
+  );
+  // {0.name: ['c', 'e']}
+  const objectDiffPrefixedKey = mapKeys(
+    objectDiffNoUnchanged,
+    (value, flatKey) => `${objectKey}.${flatKey}`
+  );
+  // {contacts.0.name: ['c', 'e']}
+  Object.assign(accumulatorObject, objectDiffPrefixedKey);
+
+  return accumulatorObject;
+}
+
+function changelogFormatTransformer(
+  accumulatorArray,
+  [valueBefore, valueAfter],
+  key
+) {
+  const label = diffFieldLabels[key] ? diffFieldLabels[key] : 'du champ ' + key;
+  const displayedValueBefore = isBoolean(valueBefore)
+    ? valueBefore
+      ? 'coché'
+      : 'décoché'
+    : valueBefore;
+  const displayedValueAfter = isBoolean(valueAfter)
+    ? valueAfter
+      ? 'coché'
+      : 'décoché'
+    : valueAfter;
+
+  accumulatorArray.push(
+    `Changement ${label} de "${displayedValueBefore}" en "${displayedValueAfter}".`
+  );
+
+  return accumulatorArray;
+}
+
+export function getChangelog(diff) {
+  return (
+    _(diff)
+      // { intitule: ['a', 'b'], contacts: [[{'name': 'c', email: 'd'}], [{'name': 'e', email: 'd'}]] }
+      .omit(['updated_at'])
+      .transform(flattenDiffTransformer, {})
+      // { intitule: ['a', 'b'], contacts.0.name: ['c', 'e'] }
+      .transform(changelogFormatTransformer, [])
+      // ['changement d'intitule de "a" en "b"', 'changement du nom du DPD de "c" en "d"']
+      .value()
+  );
 }
