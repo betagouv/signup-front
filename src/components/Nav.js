9adf31359aa6c6251dd1593e0150073fc281c4f5
Raphaël Dubigny <rdubigny@gmail.com>
2019-06-13 12:05:31 +0200

    fix the side nav + hide it for small screen

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Nav.css';
 
 const Nav = ({
   logo,
@@ -9,52 +10,50 @@ const Nav = ({
   contactInformation,
 }) => (
   <aside className="side-menu" role="navigation">
-    <ul>
-      {logo && (
-        <li>
-          <a href="https://www.impots.gouv.fr/">
-            <img
-              alt={logo.alt}
-              src={logo.src}
-              style={{ mixBlendMode: 'multiply' }}
-            />
-          </a>
-        </li>
-      )}
-      {navLinksGeneral.map(({ id, label }) => (
-        <li key={id}>
-          <a href={`#${id}`}>{label}</a>
-        </li>
-      ))}
-      {titleAdditionalContent &&
-        navLinksAdditionalContent.map(({ id, label }) => (
-          <li key={id}>
-            <a className="side-pane__link" href={`#${id}`}>
-              {label}
+    <div className="side-menu-container">
+      <ul className="form-nav">
+        {logo && (
+          <li>
+            <a href="https://www.impots.gouv.fr/">
+              <img alt={logo.alt} src={logo.src} className="form-nav-logo" />
             </a>
           </li>
+        )}
+        {navLinksGeneral.map(({ id, label }) => (
+          <li key={id}>
+            <a href={`#${id}`}>{label}</a>
+          </li>
         ))}
-    </ul>
-
-    {contactInformation && (
-      <div className="section section-grey">
-        <div className="container">
-          <h3>Une question&nbsp;?</h3>
-
-          <div className="contact-button-list">
-            {contactInformation.map(({ email, label, subject }) => (
-              <a
-                key={label}
-                className="button-outline primary"
-                href={`mailto:${email}?subject=${subject}`}
-              >
+        {titleAdditionalContent &&
+          navLinksAdditionalContent.map(({ id, label }) => (
+            <li key={id}>
+              <a className="side-pane__link" href={`#${id}`}>
                 {label}
               </a>
-            ))}
+            </li>
+          ))}
+      </ul>
+
+      {contactInformation && (
+        <div className="section section-grey help-links">
+          <div className="container">
+            <h3>Une question&nbsp;?</h3>
+
+            <div className="contact-button-list">
+              {contactInformation.map(({ email, label, subject }) => (
+                <a
+                  key={label}
+                  className="button-outline primary"
+                  href={`mailto:${email}?subject=${subject}`}
+                >
+                  {label}
+                </a>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    )}
+      )}
+    </div>
   </aside>
 );
 
