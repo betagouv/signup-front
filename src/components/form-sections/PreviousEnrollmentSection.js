ab7dceab99426fadf41d258a2654f6bcc6ce8d61
Raphaël Dubigny <rdubigny@gmail.com>
2019-10-04 17:05:42 +0200

    remove link to previous enrollement if user do not has access

--- src/components/form-sections/PreviousEnrollmentSection.js
+++ src/components/form-sections/PreviousEnrollmentSection.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ScrollablePanel } from '../elements/Scrollable';
 import ValidatedEnrollmentsSelector from '../form/ValidatedEnrollmentsSelector';
 import { TARGET_API_LABELS } from '../../pages/EnrollmentList';
+import { hasAccessToEnrollment } from '../../lib/services';
 
 const PreviousEnrollmentSection = ({
   previousTargetApi = 'franceconnect',
@@ -19,31 +20,62 @@ const PreviousEnrollmentSection = ({
   disabled = false,
   onChange = () => null,
   enrollment: { linked_franceconnect_enrollment_id = null, target_api },
-}) => (
-  <ScrollablePanel scrollableId="franceconnect">
-    <h2>Demande {TARGET_API_LABELS[previousTargetApi]} associée</h2>
-    <Description />
-    <br />
-    {!isUserEnrollmentLoading && !disabled && (
-      <ValidatedEnrollmentsSelector
-        onValidatedEnrollment={onChange}
-        linkedTargetApi={previousTargetApi}
-        enrollmentTargetApi={target_api}
-        linked_franceconnect_enrollment_id={linked_franceconnect_enrollment_id}
-      />
-    )}
-    {disabled && (
-      <div className="button-list enrollment">
-        <a
-          href={`/franceconnect/${linked_franceconnect_enrollment_id}`}
-          className="button-outline secondary"
-        >
-          Voir la demande associée
-        </a>
-      </div>
-    )}
-  </ScrollablePanel>
-);
+}) => {
+  const [
+    hasAccessToPreviousEnrollment,
+    setHasAccessToPreviousEnrollment,
+  ] = useState(false);
+
+  useEffect(() => {
+    async function fetchHasAccessToEnrollment() {
+      if (!linked_franceconnect_enrollment_id) return null;
+
+      const result = await hasAccessToEnrollment(
+        linked_franceconnect_enrollment_id
+      );
+
+      setHasAccessToPreviousEnrollment(result);
+    }
+
+    fetchHasAccessToEnrollment();
+  }, [linked_franceconnect_enrollment_id]);
+
+  return (
+    <ScrollablePanel scrollableId="franceconnect">
+      <h2>Demande {TARGET_API_LABELS[previousTargetApi]} associée</h2>
+      <Description />
+      <br />
+      {!isUserEnrollmentLoading && !disabled && (
+        <ValidatedEnrollmentsSelector
+          onValidatedEnrollment={onChange}
+          linkedTargetApi={previousTargetApi}
+          enrollmentTargetApi={target_api}
+          linked_franceconnect_enrollment_id={
+            linked_franceconnect_enrollment_id
+          }
+        />
+      )}
+      {disabled && (
+        <div className="button-list enrollment">
+          {hasAccessToPreviousEnrollment ? (
+            <a
+              href={`/franceconnect/${linked_franceconnect_enrollment_id}`}
+              className="light"
+            >
+              Numéro de la demande associée :{' '}
+              {linked_franceconnect_enrollment_id}
+            </a>
+          ) : (
+            <>
+              Numéro de la demande associée :{' '}
+              {linked_franceconnect_enrollment_id}
+            </>
+          )}
+        </div>
+      )}
+    </ScrollablePanel>
+  );
+};
 
 PreviousEnrollmentSection.propTypes = {
   previousTargetApi: PropTypes.string,
