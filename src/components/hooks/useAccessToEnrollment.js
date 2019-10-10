8c8627884f7ed56896440df1e132ae92229e038b
Raphaël Dubigny <rdubigny@gmail.com>
2019-10-10 15:15:28 +0200

    fix eslint error

--- src/components/hooks/useAccessToEnrollment.js
+++ src/components/hooks/useAccessToEnrollment.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { hasAccessToEnrollment } from '../../lib/services';
 
 const useAccessToEnrollment = enrollmentId => {
