5c9ba466b3dd1dbba733b79887da231b8956c48e
Stanislas Bernard <bernardstanislas@gmail.com>
2020-06-24 16:16:07 +0200

    :pencil: Change technical contact description
    Since the technical contact no longer receives an email from the API Particulier team, we don't need to explain it in the description anymore.

--- src/pages/ApiParticulier.js
+++ src/pages/ApiParticulier.js
@@ -49,9 +49,8 @@ const contacts = {
     heading: 'Responsable technique',
     description: () => (
       <p>
-        Cette personne recevra les accès techniques par mail. Le numéro de
-        téléphone doit être un numéro de téléphone mobile. Il sera utilisé pour
-        envoyer un code d'accès. Le responsable technique peut être le contact
+        Cette personne sera contactée en cas de problème technique sur votre
+        service. Le responsable technique peut être le contact
         technique de votre prestataire.
       </p>
     ),
