a1fff821bf9e0b490393f0b916363d0b1cb29596
Raphaël Dubigny <rdubigny@gmail.com>
2020-12-08 10:58:40 +0100

    feat: update api ficoba

--- src/pages/DgfipPages/ApiFicobaSandbox.js
+++ src/pages/DgfipPages/ApiFicobaSandbox.js
@@ -35,7 +35,7 @@ const availableScopes = [
   {
     value: 'dgfip_ficoba_compte',
     label: 'Désignation du compte',
-    checkedByDefault: true,
+    mandatory: true,
   },
   {
     value: 'dgfip_ficoba_etablissement_bancaire',
