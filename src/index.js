725dd8d6d00f49fc9207851e54e798fbb8e9e993
Xavier Jouppe <xajouppe@gmail.com>
2020-12-08 20:18:05 +0100

    Add sentry client in front

--- src/index.js
+++ src/index.js
@@ -5,5 +5,16 @@ import App from './App';
 import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 import 'moment/locale/fr'; // set moment locale to french globally
 
+// Setup sentry
+if (
+  // process.env.NODE_ENV === 'production' &&
+  typeof window !== 'undefined' &&
+  !!window.Raven
+) {
+  window.Raven.config(
+    'https://40fcfa878949435bb8ff723e618b395d@sentry.data.gouv.fr/64'
+  ).install();
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 unregisterServiceWorker();
