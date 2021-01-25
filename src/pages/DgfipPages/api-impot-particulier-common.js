7d55465085892af72eae12119005e5d24b27a1cf
Raphaël Dubigny <rdubigny@gmail.com>
2021-01-25 12:14:06 +0100

    docs: update DGFiP doc

--- src/pages/DgfipPages/api-impot-particulier-common.js
+++ src/pages/DgfipPages/api-impot-particulier-common.js
@@ -92,7 +92,7 @@ export const DonneesFootnote = () => (
     <small className="card__meta">
       <i>
         <a
-          href="/docs/presentation_de_l_api_impot_particulier___v1.6.pdf"
+          href="/docs/presentation_de_l_api_impot_particulier___v1.6.1.pdf"
           target="_blank"
           rel="noreferrer noopener"
           aria-label="Document pdf précisant les données proposées"
