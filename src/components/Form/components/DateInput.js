319a8d98071ce5dad926f7aab482f8cad46f24bc
Raphaël Dubigny <rdubigny@gmail.com>
2021-02-04 12:12:53 +0100

    fix: on Safari and IE11 dates can at the wrong format

--- src/components/Form/components/DateInput.js
+++ src/components/Form/components/DateInput.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Input from './input';
 
-export const DateInput = props => <Input {...props} type="date" />;
+export const DateInput = props => (
+  // the placeholder is used only on Safari and IE11 where input[type="date"] is not implemented.
+  <Input {...props} placeholder={'AAAA-MM-JJ'} type="date" />
+);
 
 export default DateInput;
