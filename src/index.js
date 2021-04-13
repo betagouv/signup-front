a52ba320ecb29c52a2c32af65aabbce903d9a080
Raphaël Dubigny <rdubigny@gmail.com>
2021-04-13 17:45:43 +0200

    feat: update sentry link

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Raven from 'raven-js';
 // Setup sentry
 if (process.env.NODE_ENV === 'production') {
   Raven.config(
-    'https://40fcfa878949435bb8ff723e618b395d@sentry.data.gouv.fr/64'
+    'https://7ccf44097ef645f8835408b9d73e7781@sentry.data.gouv.fr/8'
   ).install();
 }
 
