d674bfa25738b4f84a42a839dde3c555e1bd8749
Raphaël Dubigny <rdubigny@gmail.com>
2021-04-14 18:29:52 +0200

    feat: udpate api home plus

--- src/pages/ApiHomePlus/index.js
+++ src/pages/ApiHomePlus/index.js
@@ -69,6 +69,104 @@ const availableScopes = [
   },
 ];
 
+const ModaliteUtilisationSection = () => {
+  const {
+    disabled,
+    onChange,
+    enrollment: { documents = [], documents_attributes = [] },
+  } = useContext(FormContext);
+
+  return (
+    <ScrollablePanel scrollableId="cgu">
+      <h2>Modalités d’utilisation</h2>
+      <Quote>
+        <p>
+          Afin de vérifier que vous êtes en pleine connaissance de vos
+          obligations fiscales (paiement de la TVA et de l’impôt sur le revenu
+          ou sur les sociétés selon votre situation), merci de joindre une
+          attestation fiscale justifiant de la régularité de votre situation
+          fiscale.
+        </p>
+      </Quote>
+      <FileInput
+        disabled={disabled}
+        uploadedDocuments={documents}
+        documentsToUpload={documents_attributes}
+        documentType={'Document::DecisionHomologation'}
+        handleChange={onChange}
+        label={'Attestation fiscale'}
+      />
+      <Quote>
+        <p>
+          Afin de vérifier que vous êtes habilité à fournir des prestations de
+          services à domicile, merci de joindre un document justifiant que vous
+          êtes agréé dans les conditions prévues à l’article L. 7232-1 du code
+          du travail, que vous procédez à la déclaration prévue à l’article L.
+          7232-1-1 du code du travail ou que vous disposez d’une autorisation en
+          cours de validité pour exercer les activités relevant du I de
+          l’article D. 312-6-2 du code de l’action sociale et des familles
+        </p>
+      </Quote>
+      <FileInput
+        disabled={disabled}
+        uploadedDocuments={documents}
+        documentsToUpload={documents_attributes}
+        documentType={'Document::DecisionHomologation'}
+        handleChange={onChange}
+        label={'Habilitation service à domicile'}
+      />
+    </ScrollablePanel>
+  );
+};
+
+const ContratDeLicenceSection = () => {
+  const {
+    disabled,
+    onChange,
+    enrollment: { documents = [], documents_attributes = [] },
+  } = useContext(FormContext);
+
+  return (
+    <ScrollablePanel scrollableId="licence">
+      <h2>Contrat de Licence</h2>
+      <Quote>
+        <p>
+          La signature devra impérativement être réalisée électroniquement par
+          un dispositif certifié.
+        </p>
+        <p>
+          Dans le cas où votre organisme n’aurait pas de dispositif de signature
+          électronique, l’outil Docusign (
+          <a
+            href={'https://www.docusign.fr'}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://www.docusign.fr
+          </a>
+          ) offre par exemple un accès de 30 jours à sa plateforme de signature.
+        </p>
+        <p>
+          Il vous suffira de cliquer sur « Essai gratuit » en haut à droite de
+          la page Web, de créer un compte et de l’activer. Une fois cela fait,
+          vous devrez déposer votre document sur la page, cocher la case « Je
+          suis le seul signataire » et suivre les instructions. Vous pourrez
+          utiliser le champ « Signature » présent dans le menu à gauche pour
+          déposer votre signature sur le document
+        </p>
+      </Quote>
+      <FileInput
+        disabled={disabled}
+        uploadedDocuments={documents}
+        documentsToUpload={documents_attributes}
+        documentType={'Document::DecisionHomologation'}
+        handleChange={onChange}
+        label={'Contrat de licence signé'}
+      />
+    </ScrollablePanel>
+  );
+};
+
 const target_api = 'api_home_plus';
 
 const ApiHomePlus = ({
