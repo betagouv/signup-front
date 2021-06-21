a85b7fac79bee36dee4e28e781c78173c2a09bf9
Stanislas Bernard <bernardstanislas@gmail.com>
2021-06-21 16:38:02 +0200

    feat: Split API Particulier DGFIP scopes

--- src/pages/ApiParticulier/index.js
+++ src/pages/ApiParticulier/index.js
@@ -133,44 +133,170 @@ const DonneesDescription = () => (
 
 const availableScopes = [
   {
-    value: 'dgfip_avis_imposition',
-    label: 'DGFIP - Avis d’imposition',
+    value: 'dgfip_declarant1_nom',
+    label: 'Nom',
+    groupTitle: 'DGFIP - État civil - déclarant 1',
   },
   {
-    value: 'dgfip_adresse',
-    label: 'DGFIP - Adresse',
+    value: 'dgfip_declarant1_nom_naissance',
+    label: 'Nom de naissance',
+    groupTitle: 'DGFIP - État civil - déclarant 1',
+  },
+  {
+    value: 'dgfip_declarant1_prenoms',
+    label: 'Prénom(s)',
+    groupTitle: 'DGFIP - État civil - déclarant 1',
+  },
+  {
+    value: 'dgfip_declarant1_date_naissance',
+    label: 'Date de naissance',
+    groupTitle: 'DGFIP - État civil - déclarant 1',
+  },
+  {
+    value: 'dgfip_declarant2_nom',
+    label: 'Nom',
+    groupTitle: 'DGFIP - État civil - déclarant 2',
+  },
+  {
+    value: 'dgfip_declarant2_nom_naissance',
+    label: 'Nom de naissance',
+    groupTitle: 'DGFIP - État civil - déclarant 2',
+  },
+  {
+    value: 'dgfip_declarant2_prenoms',
+    label: 'Prénom(s)',
+    groupTitle: 'DGFIP - État civil - déclarant 2',
+  },
+  {
+    value: 'dgfip_declarant2_date_naissance',
+    label: 'Date de naissance',
+    groupTitle: 'DGFIP - État civil - déclarant 2',
+  },
+  {
+    value: 'dgfip_date_recouvrement',
+    label: 'Date de recouvrement',
+    groupTitle: "DGFIP - Échéances de l'avis d'imposition",
+  },
+  {
+    value: 'dgfip_date_etablissement',
+    label: "Date d'établissement",
+    groupTitle: "DGFIP - Échéances de l'avis d'imposition",
+  },
+  {
+    value: 'dgfip_adresse_fiscale_taxation',
+    label: 'Adresse',
+    groupTitle: 'DGFIP - Situation du foyer fiscal',
+  },
+  {
+    value: 'dgfip_adresse_fiscale_annee',
+    label: 'Année de déclaration',
+    groupTitle: 'DGFIP - Situation du foyer fiscal',
+  },
+  {
+    value: 'dgfip_nombre_parts',
+    label: 'Nombre de parts',
+    groupTitle: 'DGFIP - Situation du foyer fiscal',
+  },
+  {
+    value: 'dgfip_nombre_personnes_a_charge',
+    label: 'Nombre de personnes à charge',
+    groupTitle: 'DGFIP - Situation du foyer fiscal',
+  },
+  {
+    value: 'dgfip_situation_familiale',
+    label: 'Situation de famille',
+    groupTitle: 'DGFIP - Situation du foyer fiscal',
+    helper: '(marié, pacsé, célibataire, veuf divorcé)',
+  },
+  {
+    value: 'dgfip_revenu_brut_global',
+    label: 'Revenu brut global',
+    groupTitle: 'DGFIP - Agrégats fiscaux',
+  },
+  {
+    value: 'dgfip_revenu_imposable',
+    label: 'Revenu imposable',
+    groupTitle: 'DGFIP - Agrégats fiscaux',
+  },
+  {
+    value: 'dgfip_impot_revenu_net_avant_corrections',
+    label: 'Impôt sur le revenu net avant corrections',
+    groupTitle: 'DGFIP - Agrégats fiscaux',
+  },
+  {
+    value: 'dgfip_montant_impot',
+    label: "Montant de l'impôt",
+    groupTitle: 'DGFIP - Agrégats fiscaux',
+  },
+  {
+    value: 'dgfip_revenu_fiscal_reference',
+    label: 'Revenu fiscal de référence',
+    groupTitle: 'DGFIP - Agrégats fiscaux',
+  },
+  {
+    value: 'dgfip_annee_impot',
+    label: "Année de l'impôt",
+    groupTitle: 'DGFIP - Agrégats fiscaux',
+  },
+  {
+    value: 'dgfip_annee_revenus',
+    label: 'Année des revenus',
+    groupTitle: 'DGFIP - Agrégats fiscaux',
+  },
+  {
+    value: 'dgfip_erreur_correctif',
+    label: 'Erreur correctif',
+    groupTitle: 'DGFIP - Compléments',
+    helper:
+      "Indique si un correctif plus récent que l'avis recherché est disponible.",
+  },
+  {
+    value: 'dgfip_situation_partielle',
+    label: 'Situation partielle',
+    groupTitle: 'DGFIP - Compléments',
+    helper:
+      "Dans un foyer marié ou pacsé, quand décès d'un des contribuable, affiche les données de l'avis avec l’indication « situation partielle ». Donc, il faut les références de l'autre avis pour le consulter.",
   },
   {
     value: 'cnaf_quotient_familial',
-    label: 'CNAF - Quotient familial',
+    label: 'Quotient familial',
+    groupTitle: 'CNAF',
   },
   {
     value: 'cnaf_allocataires',
-    label: 'CNAF - Allocataires',
+    label: 'Allocataires',
+    groupTitle: 'CNAF',
   },
   {
     value: 'cnaf_enfants',
-    label: 'CNAF - Enfants',
+    label: 'Enfants',
+    groupTitle: 'CNAF',
   },
   {
     value: 'cnaf_adresse',
-    label: 'CNAF - Adresse',
+    label: 'Adresse',
+    groupTitle: 'CNAF',
   },
   {
     value: 'pole_emploi_identite',
-    label: 'Pôle Emploi - Identité',
+    label: 'Identité',
+    groupTitle: 'Pôle Emploi',
   },
   {
     value: 'pole_emploi_contact',
-    label: 'Pôle Emploi - Données de contact',
+    label: 'Données de contact',
+    groupTitle: 'Pôle Emploi',
   },
   {
     value: 'pole_emploi_adresse',
-    label: 'Pôle Emploi - Adresse',
+    label: 'Adresse',
+    groupTitle: 'Pôle Emploi',
   },
   {
     value: 'pole_emploi_inscription',
-    label: 'Pôle Emploi - Inscription',
+    label: 'Inscription',
+    groupTitle: 'Pôle Emploi',
+    helper: "Données concernant l'inscription de la personne à Pôle Emploi",
   },
 ];
 
