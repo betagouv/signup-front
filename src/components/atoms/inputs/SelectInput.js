4c9e25f68b4f05b99600a75a6e4a8a25eac574ed
Raphaël Dubigny <rdubigny@gmail.com>
2021-09-10 18:23:22 +0200

    feat: introduce other option for select

--- src/components/atoms/inputs/SelectInput.js
+++ src/components/atoms/inputs/SelectInput.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import Helper from '../Helper';
+import { uniqueId } from 'lodash';
+import TextInput from './TextInput';
+import SideBySideWrapper from './SideBySideWrapper';
 
-export const Select = ({
+export const SelectInput = ({
   label,
   name,
   helper,
@@ -9,27 +12,58 @@ export const Select = ({
   value,
   disabled,
   onChange,
+  required,
   useOtherOption = false,
-}) => (
-  <div className="form__group">
-    <label htmlFor={name}>
-      {label}
-      {helper && <Helper title={helper} />}
-    </label>
-    <select
-      id={name}
-      name={name}
-      value={value}
-      disabled={disabled}
-      onChange={onChange}
-    >
-      {options.map(({ id, label: optionLabel }) => (
-        <option key={id} value={id}>
-          {optionLabel}
-        </option>
-      ))}
-    </select>
-  </div>
-);
+}) => {
+  // id will be set once when the component initially renders, but never again
+  // we generate an unique id prefixed by the field name
+  const [id] = useState(uniqueId(name));
 
-export default Select;
+  const isOtherSelected = useMemo(
+    () => useOtherOption && !options.map(({ id: i }) => i).includes(value),
+    [useOtherOption, options, value]
+  );
+
+  return (
+    <SideBySideWrapper>
+      <div className="form__group">
+        <label htmlFor={id}>
+          {label}
+          {required && ' *'}
+          {helper && <Helper title={helper} />}
+        </label>
+        <select
+          id={id}
+          name={name}
+          value={isOtherSelected ? '' : value}
+          disabled={disabled}
+          onChange={onChange}
+          required={required}
+        >
+          {options.map(({ id, label: optionLabel }) => (
+            <option key={id} value={id}>
+              {optionLabel}
+            </option>
+          ))}
+          {useOtherOption && (
+            <option key="" value="">
+              Autre
+            </option>
+          )}
+        </select>
+      </div>
+      {isOtherSelected && (
+        <TextInput
+          label="Précisez :"
+          name={name}
+          value={value}
+          disabled={disabled}
+          onChange={onChange}
+          ariaLabel={`Précisez le ${label}`}
+        ></TextInput>
+      )}
+    </SideBySideWrapper>
+  );
+};
+
+export default SelectInput;
