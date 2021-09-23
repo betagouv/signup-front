9be1988951bc32e15a9eb748fd6cbd42289a909c
etienne0101 <79003454+etienne0101@users.noreply.github.com>
2021-09-23 14:23:50 +0200

    Create ApiSystemeImmatriculationVehicules.js

---
+++ src/pages/ApiSystemeImmatriculationVehicules.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Form from '../../components/templates/Form';
+import OrganisationSection from '../../components/organisms/form-sections/OrganisationSection';
+import DemarcheSection from '../../components/organisms/form-sections/DemarcheSection';
+import DescriptionSection from '../../components/organisms/form-sections/DescriptionSection';
+import DonneesSection from '../../components/organisms/form-sections/DonneesSection';
+import CadreJuridiqueSection from '../../components/organisms/form-sections/CadreJuridiqueSection';
+import CguSection from '../../components/organisms/form-sections/CguSection';
+import demarches from './demarches.json';
+import ÉquipeSection from '../../components/organisms/form-sections/ÉquipeSection';
+
+const DonneesDescription = () => (
+  <>
+    <p>
+      La loi informatique et libertés définit les principes à respecter lors de
+      la collecte, du traitement et de la conservation de données personnelles.
+    </p>
+    <p>L’article 6 précise :</p>
+    <ul>
+      <li>
+        3° [les données] sont adéquates, pertinentes et non excessives au regard
+        des finalités pour lesquelles elles sont collectées et de leurs
+        traitements ultérieurs ;
+      </li>
+      <li>
+        4° Elles sont exactes, complètes et, si nécessaire, mises à jour ; les
+        mesures appropriées doivent être prises pour que les données inexactes
+        ou incomplètes au regard des finalités pour lesquelles elles sont
+        collectées ou traitées soient effacées ou rectifiées ;
+      </li>
+    </ul>
+    <p>
+      Nous vous remercions de sélectionner uniquement les données strictement
+      nécessaires à votre téléservice. Le non-respect du principe de
+      proportionnalité vous expose vis-à-vis de la CNIL.
+    </p>
+  </>
+);
+
+const CadreJuridiqueDescription = () => (
+  <>
+    <p>
+      Pour pouvoir bénéficier du raccordement à l’API SIV, le cadre légal et
+      réglementaire des fournisseurs de service doit permettre au Ministère de
+      l'Intérieur de transmettre des données personnelles à votre entité
+      administrative.
+    </p>
+    <p>
+      Dans le cas où vous représentez une collectivité, veuillez joindre la
+      délibération du conseil municipal explicitant l’usage des données
+      demandées.
+    </p>
+    <p>
+      Important : à priori, vous n’avez pas de nouvelle délibération à réaliser.
+      Il vous suffit de déposer la dernière délibération tarifaire qui détaille
+      les barèmes de facturation des services, ou le règlement qui décrit les
+      données nécessaires à une instruction.
+    </p>
+  </>
+);
+
+const availableScopes = [
+  {
+    value: 'a_completer',
+    label: 'à compléter',
+    mandatory: true,
+  },
+];
+
+const target_api = 'api_particulier';
+
+const ApiParticulier = ({
+  match: {
+    params: { enrollmentId },
+  },
+}) => (
+  <Form
+    enrollmentId={enrollmentId}
+    target_api={target_api}
+    demarches={demarches}
+    contactInformation={[
+      {
+        email: 'contact@particulier.api.gouv.fr',
+        label: 'Nous contacter',
+        subject: 'Contact%20via%20datapass.api.gouv.fr',
+      },
+    ]}
+  >
+    <OrganisationSection />
+    <DemarcheSection />
+    <DescriptionSection />
+    <DonneesSection
+      availableScopes={availableScopes}
+      DonneesDescription={DonneesDescription}
+    />
+    <CadreJuridiqueSection
+      CadreJuridiqueDescription={CadreJuridiqueDescription}
+    />
+    <ÉquipeSection />
+    <CguSection cguLink="https://api.gouv.fr/resources/CGU%20API%20Particulier.pdf" />
+  </Form>
+);
+
+ApiParticulier.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      enrollmentId: PropTypes.string,
+    }),
+  }),
+};
+
+ApiParticulier.defaultProps = {
+  match: {
+    params: {
+      enrollmentId: null,
+    },
+  },
+};
+
+export default ApiParticulier;
